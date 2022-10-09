f2ce64447993a86673c976058da0b07569d08d43
Sammy <marchsam1988@gmail.com>
2022-10-09 13:23:26 +1030

    thought model and reaction virtual

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,49 @@
 const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+    },
+  ],
+});
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
