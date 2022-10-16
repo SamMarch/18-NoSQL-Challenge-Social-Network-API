79ff612e244ad7e8ef4cca3a584f7b64fa20adf6
Sammy <marchsam1988@gmail.com>
2022-10-16 16:04:18 +1030

    seed logic changed to fix thoughts not populating

--- utils/seed.js
+++ utils/seed.js
@@ -8,10 +8,18 @@ connection.once("open", async () => {
   try {
     await connection.db.dropDatabase();
     await User.create(users);
-    await Thought.create(thoughts);
+    const createdThoughts = await Thought.create(thoughts, { new: true });
+    createdThoughts.forEach(async (thought) => {
+      console.log(thought);
+      await User.findOneAndUpdate(
+        { username: thought.username },
+        { $addToSet: { thoughts: thought._id } },
+        { new: true }
+      );
+    });
     // await User.updateMany({}, { $set: { friends: friends } });
     console.log("Database seeded!");
-    process.exit(0);
+    // process.exit(0);
   } catch (err) {
     console.error(err);
     process.exit(1);
